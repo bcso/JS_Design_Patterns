5c6f39f51f96d9d892721a9f4f9660b7a6c12dd8
bcso <bcso@uwaterloo.ca>
2015-01-11 18:03:31 -0500

    added individual counters

--- CatClicker/js/click.js
+++ CatClicker/js/click.js
@@ -1,10 +1,15 @@
 $(document).ready(function(){
     var count = 0;
     
-    $('.cat').click(function(e){
+    $('#catImg1').click(function(e){
         count++;        
-        $("#clicks").text(count);
+        $("#clicks1").text(count);
     });
+    
+    $('#catImg2').click(function(e){
+        count++;        
+        $("#clicks2").text(count);
+    });    
     
     $("#catName1").text("Tay");
     $("#catName2").text("Jessica");
