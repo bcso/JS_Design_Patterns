2771c9e2149975ec929fb63f3bf9370f7f97b325
bcso <bcso@uwaterloo.ca>
2015-01-11 20:01:56 -0500

    added cat nav bar and cat counts (meow!)

--- CatClicker/js/click.js
+++ CatClicker/js/click.js
@@ -1,16 +1,52 @@
+var num = 3;
+var nums = new Array(num);
+var names = ["Tay", "Jessica", "George"];
+var prev = 0;
+
 $(document).ready(function(){
-    var count = 0;
     
-    $('#catImg1').click(function(e){
-        count++;        
-        $("#clicks1").text(count);
-    });
+    //Attach all the listeners and display all images
+    addCats(nums);
     
-    $('#catImg2').click(function(e){
-        count++;        
-        $("#clicks2").text(count);
-    });    
-    
-    $("#catName1").text("Tay");
-    $("#catName2").text("Jessica");
+    //Initiate the array of clicks
+    for (var i = 0; i < nums.length; i++){
+        nums[i] = 0;
+    }
 })
+
+var addCats = function(nums){
+    
+    
+    for (var i = 0; i<nums.length; i++){
+        $(".catsBar").append('<img src="res/meow' + i + '.jpg" id="catImg' + i + 'bar">');
+        $(".catsMain").append('<div id = "clicks' + i + '"></div><p class="catName" id="catName' + i + '"></p><img src="res/meow' + i + '.jpg" id="catImg' + i + 'main">');        
+        $("#catImg" + i + "main").hide();
+        
+        //add the click listener
+        $("#catImg" + i + "bar").click(function(e){            
+            var $selection = $(this).attr('id');
+            var catID = parseInt($selection.substr(6,7)); //get the image ID
+            
+            hideAllImgs();
+            
+            updateClick(catID);            
+            
+            $("#" + $selection.substr(0,7) + "main").show();
+            $("#clicks" + catID).show();
+        });
+    }
+};
+
+//Increment the click count and draw it for specified image
+var updateClick = function(catID){
+    nums[catID] = nums[catID] + 1;   
+    
+    $("#clicks" + catID ).text(names[catID] + ": " + nums[catID]);
+};
+
+var hideAllImgs = function(){
+    for (var i = 0; i < nums.length; i ++){
+        $("#catImg" + i + "main").hide();
+        $("#clicks" + i).hide();
+    }
+}
